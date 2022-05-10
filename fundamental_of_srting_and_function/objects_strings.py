f9911961cb12ab05281e826a987d5be21f84688f
Astiyaxh <63425854+Astiyaxh@users.noreply.github.com>
2022-05-10 09:53:41 +0430

    Create objects_strings.py

---
+++ fundamental_of_srting_and_function/objects_strings.py
@@ -0,0 +1,20 @@
+"sushi"
+'sushi'
+"waeeiosfan123!"
+"$5.00"
+
+"You're right"
+'shakespeare once wrote "To be or not to be" in one of his plays'
+
+" "
+""
+''
+
+"5"
+5
+
+"""hello
+my name is
+iran
+"""
+
